fd022f8b7c8aeb99ca85db6a72616543ce37562f
thodkatz <thodkatz@gmail.com>
2021-10-01 18:46:12 +0300

    Change testing workflow

--- test/spgemm.py
+++ test/spgemm.py
@@ -1,25 +1,71 @@
 from scipy.sparse import coo_matrix, csr_matrix, csc_matrix, find
 from scipy.io import mmread
+from pytictoc import TicToc
+from termcolor import colored
 import numpy as np
 import sys
-from termcolor import colored
 import time
-from pytictoc import TicToc
 
 
-if(len(sys.argv) != 2):
-    print("Usage: python spgemm.py <mtx filename>")
+if(len(sys.argv) != 4):
+    print("Usage: python spgemm.py <mtx filename A> <mtx filename B> <mtx filename C>")
     exit()
 
 # Python SpGEMM
 
-coo = mmread(sys.argv[1])
-csr = coo.tocsr()
+#cooA = mmread(sys.argv[1])
+
+# Read MM format
+# Skip banner
+def skipBanner(filename):
+    readFile = open(filename, 'r')
+    lines = readFile.readlines()
+    numComments = 0;
+    for line in lines:
+        if line[0] == "%":
+            numComments = numComments+1
+        else:
+            metadata = [int(i) for i in line.split(" ")]
+            numRow, numCol, nnz = metadata[0], metadata[1], metadata[2]
+            break
+
+    return numRow, numCol, nnz, numComments
+
+numRowA, numColA, nnzA, numCommentsA = skipBanner(sys.argv[1])
+numRowB, numColB, nnzB, numCommentsB = skipBanner(sys.argv[2])
+numRowF, numColF, nnzF, numCommentsF = skipBanner(sys.argv[3])
+
+mmA = np.genfromtxt(sys.argv[1], delimiter=' ', skip_header=numCommentsA+1)
+mmB = np.genfromtxt(sys.argv[2], delimiter=' ', skip_header=numCommentsB+1)
+mmF = np.genfromtxt(sys.argv[3], delimiter=' ', skip_header=numCommentsF+1)
+
+rowA,colA = mmA[:,0], mmA[:,1]
+rowB,colB = mmB[:,0], mmB[:,1]
+rowF,colF = mmF[:,0], mmF[:,1]
+
+# adjust index
+rowA = rowA - 1
+colA = colA - 1
+rowB = rowB - 1
+colB = colB - 1
+rowF = rowF - 1
+colF = colF - 1
+
+cooA = coo_matrix((np.ones(nnzA), (rowA,colA)), shape=(numRowA, numColA))
+cooB = coo_matrix((np.ones(nnzB), (rowB,colB)), shape=(numRowB, numColB))
+cooF = coo_matrix((np.ones(nnzF), (rowB,colF)), shape=(numRowF, numColF))
+
+csrA = cooA.tocsr()
+csrB = cooB.tocsr()
+csrF = cooF.tocsr()
 
 t = TicToc()
 t.tic()
-csrMulRef = (csr*csr>0)*1
+csrMulRef = csrF.multiply(csrA*csrB)
+csrMulRef = (csrMulRef>0)*1
 t.toc()
+
+print("Reference C.nnz: " + str(csrMulRef.getnnz()))
 
 # C++ SpGEMM
 
@@ -27,8 +73,20 @@ matFile="csrMul.txt"
 csrFile = open(matFile)
 lines = csrFile.readlines()
 csrFile.close()
+
+# check special case, zero C matrix
+if(len(lines) == 1):
+    pointer = np.fromstring(lines[0],dtype=int, sep=',')
+    if(not np.array_equiv(pointer,csrMulRef.indptr)):
+        print(colored("Test failed :(","red", attrs=["bold"]))
+        print("Pointer arrays are different")
+        exit()
+    else:
+        print(colored("Hooray! Test passed :)", "green", attrs=["bold"]))
+        exit()
+
 if(len(lines) != 2):
-    print("Invalid file\n")
+    print("Couldn't parse C-CSR file")
     exit()
 
 pointer = np.fromstring(lines[0],dtype=int, sep=',')
@@ -43,10 +101,18 @@ csrMul = csr_matrix((data, indices, pointer), shape=(n-1,n-1))
 if(csrMul.getnnz() != csrMulRef.getnnz()):
     print(colored("Test failed :(","red", attrs=["bold"]))
     print(colored("Incompatible size"))
+    print("Reference: " + str(csrMulRef.getnnz()))
+    print("Got: " + str(csrMul.getnnz()))
+    exit()
 
-csrDiffs = find(csrMul-csrMulRef)
-if len(csrDiffs[0])==0 and len(csrDiffs[1])==0 and len(csrDiffs[2])==0:
-    print(colored("Hooray! Test passed","green", attrs=["bold"]))
-else:
-    print(find(csrDiff))
+if(not np.array_equiv(csrMul.indptr,csrMulRef.indptr)):
     print(colored("Test failed :(","red", attrs=["bold"]))
+    print("Pointer arrays are different")
+    exit()
+
+if(not np.array_equiv(csrMul.indices,csrMulRef.indices)):
+    print(colored("Test failed :(","red", attrs=["bold"]))
+    print("Pointer arrays are different")
+    exit()
+
+print(colored("Hooray! Test passed :)", "green", attrs=["bold"]))
--- test/validation.sh
+++ test/validation.sh
@@ -6,28 +6,52 @@ MATRICES=$PROJECT/matrices
 figlet Build | lolcat
 cd ../ && make serial
 
-#matrices=("jgl009/jgl009.mtx" "mycielskian3/mycielskian3.mtx" "mycielskian10/mycielskian10.mtx")
+A="A.mtx"
+B="B.mtx"
+F="F.mtx"
+
+#A="mycielskian3/mycielskian3.mtx"
+#A="jgl009/jgl009.mtx"
+B=$A
+F=$A
+
+FILEA=$MATRICES/$A
+FILEB=$MATRICES/$B
+FILEF=$MATRICES/$F
+
+echo -e "\nFull file path matrix A: $FILEA"
+echo -e "\nFull file path matrix B: $FILEB"
+echo -e "\nFull file path matrix F: $FILEF"
+
+figlet Run | lolcat
+./bin/serial $FILEA $FILEB $FILEF
+
+figlet Validation | lolcat
+cd test/ && python spgemm.py $FILEA $FILEB $FILEF && cd ../
+
+# matrices=("jgl009/jgl009.mtx" "mycielskian3/mycielskian3.mtx" "mycielskian10/mycielskian10.mtx")
 #matrices=("roadNet-PA/roadNet-PA.mtx")
 #matrices=("mycielskian10/mycielskian10.mtx")
 #matrices=("Stanford/Stanford.mtx")
 #matrices=("G47/G47.mtx")
 #matrices=("wing_nodal/wing_nodal.mtx")
 #matrices=("biplane-9/biplane-9.mtx")
-matrices=("pli/pli.mtx")
+#matrices=("pli/pli.mtx")
 #matrices=("dblp-2010/dblp-2010.mtx")
 
-for i in "${matrices[@]}"
-do
-    FILE=$MATRICES/$i
-    echo -e "\nFull file path: $FILE"
-    pwd
+# for i in "${matrices[@]}"
+# do
+#     FILE=$MATRICES/$i
+#     echo -e "\nFull file path: $FILE"
+#     pwd
 
-    # to be compatible with the matrix market reading of the C++ program we assume that symmetric is general
-    sed -i '/symmetric/ s//general/g' $FILE
+#     # to be compatible with the matrix market reading of the C++ program we assume that symmetric is general
+#     sed -i '/symmetric/ s//general/g' $FILE
 
-    figlet Run | lolcat
-    ./bin/serial $FILE
+#     figlet Run | lolcat
+#     ./bin/serial $FILE $FILE
 
-    figlet Validation | lolcat
-    cd test/ && python spgemm.py $FILE && cd ../
-done
+#     figlet Validation | lolcat
+#     cd test/ && python spgemm.py $FILE && cd ../
+
+#done
